aa6a86016067e28606deb5ba32dcce4c01c73024
spenserg92 <spenserg92@gmail.com>
2024-01-19 16:03:31 -0500

    added index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Pet
 
 # Create your views here.
 
@@ -10,3 +11,11 @@ def home(request):
 def about(request):
     # Include an .html file extension - unlike when rendering EJS templates
     return render(request, 'about.html')
+
+def pets_index(request):
+    #collect our objects from the db
+    # cats = Cat.objects.all()
+    pets = Pet.objects.filter(user=request.user)
+    # You could also retrieve the logged in user's cats like this
+    # cats = request.user.cat_set.all()
+    return render(request, 'pets/index.html', { 'pets': pets })
