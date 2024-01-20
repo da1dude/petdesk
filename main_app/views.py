f0a7868f065d1b0f236c2166d04eafc432bee3de
albi <albijon.re@gmail.com>
2024-01-20 13:38:47 -0500

    delete funcunality and species drop down

--- main_app/views.py
+++ main_app/views.py
@@ -68,3 +68,7 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+class PetDelete(LoginRequiredMixin, DeleteView):
+    model = Pet
+
+    success_url = '/pets'
