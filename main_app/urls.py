fa1f2c5882baecbc9635c35bcffb6b5236d99cff
spenserg92 <spenserg92@gmail.com>
2024-01-20 15:39:47 -0500

    adds in changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,11 +7,8 @@ urlpatterns = [
     path('pets/', views.pets_index, name='index'),
     path('pets/<int:pet_id>/', views.pets_detail, name='detail'),
     path('pets/create/', views.PetCreate.as_view(), name='pets_create'),
-<<<<<<< HEAD
     path('pets/<int:pk>/update/', views.PetUpdate.as_view(), name='pets_update'),
-=======
     path('pets/<int:pk>/delete', views.PetDelete.as_view(), name='pets_delete'),
->>>>>>> e7253d098797c889e5df748fc27496e250869b4b
 
 
     #Auth URLs
