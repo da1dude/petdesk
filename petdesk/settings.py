93bf1e92dcc851fea046b632c71fd50239ba1421
Paul K <pkerschieter@gmail.com>
2024-01-25 12:41:01 -0600

    getting ready for deploy

--- petdesk/settings.py
+++ petdesk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7cpmrb8055b6(k+wm+@ohu4y6!07fs98vz34&r9pv6^$4zgg!h'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
