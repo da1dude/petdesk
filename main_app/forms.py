1e0d7b63755c07ae2c41016a6b421be275a07ffc
Paul K <pkerschieter@gmail.com>
2024-01-25 13:29:18 -0600

    trying to deploy

--- main_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Checkin, Rx
-
-class CheckinForm(ModelForm):
-    class Meta:
-        model = Checkin
-        fields = ['date', 'room', 'reason']
-
